710570036de0210dea84ca66d49029764adfd34b
Soraphis <soraphis@googlemail.com>
2019-02-25 15:16:31 +0100

    Fixes #11 - Added Equality Members (inclusive HashCode) for ScriptableObjectVariableBase

--- Assets/UnityAtoms/Base/Variable/ScriptableObjectVariableBase.cs
+++ Assets/UnityAtoms/Base/Variable/ScriptableObjectVariableBase.cs
@@ -13,5 +13,25 @@ namespace UnityAtoms
 
         [SerializeField]
         protected T value;
+
+        protected bool Equals(ScriptableObjectVariableBase<T> other) {
+            return EqualityComparer<T>.Default.Equals(value, other.value);
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ScriptableObjectVariableBase<T>) obj);
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                return EqualityComparer<T>.Default.GetHashCode(value);
+            }
+        }
+
+        public static bool operator ==(ScriptableObjectVariableBase<T> left, ScriptableObjectVariableBase<T> right) { return Equals(left, right); }
+        public static bool operator !=(ScriptableObjectVariableBase<T> left, ScriptableObjectVariableBase<T> right) { return !Equals(left, right); }
     }
 }
