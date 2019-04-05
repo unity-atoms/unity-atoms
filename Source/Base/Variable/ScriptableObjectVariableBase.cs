547bb74ec5646ea127d103640b7368d3d8f797d0
Soraphis <soraphis@googlemail.com>
2019-04-06 01:44:10 +0200

    scriptablevariables now discard playmode changes.

--- Source/Base/Variable/ScriptableObjectVariableBase.cs
+++ Source/Base/Variable/ScriptableObjectVariableBase.cs
@@ -5,19 +5,20 @@ using UnityEngine.Serialization;
 
 namespace UnityAtoms
 {
-    public abstract class ScriptableObjectVariableBase<T> : ScriptableObject, IWithValue<T>
+    public abstract class ScriptableObjectVariableBase<T> : ScriptableObject, IWithValue<T>, ISerializationCallbackReceiver
     {
-        public virtual T Value { get { return _value; } set { throw new NotImplementedException(); } }
+        public virtual T Value { get { return _runtimeValue; } set { throw new NotImplementedException(); } }
 
         [Multiline]
         public string DeveloperDescription = "";
 
+
         [FormerlySerializedAs("value")]
-        [SerializeField]
-        protected T _value;
+        [SerializeField] protected T _initialValue;
+        [NonSerialized] protected T _runtimeValue;
 
         protected bool Equals(ScriptableObjectVariableBase<T> other) {
-            return EqualityComparer<T>.Default.Equals(_value, other._value);
+            return EqualityComparer<T>.Default.Equals(_runtimeValue, other._runtimeValue);
         }
 
         public override bool Equals(object obj) {
@@ -29,11 +30,14 @@ namespace UnityAtoms
 
         public override int GetHashCode() {
             unchecked {
-                return EqualityComparer<T>.Default.GetHashCode(_value);
+                return EqualityComparer<T>.Default.GetHashCode(_runtimeValue);
             }
         }
 
         public static bool operator ==(ScriptableObjectVariableBase<T> left, ScriptableObjectVariableBase<T> right) { return Equals(left, right); }
         public static bool operator !=(ScriptableObjectVariableBase<T> left, ScriptableObjectVariableBase<T> right) { return !Equals(left, right); }
+
+        public void OnBeforeSerialize() {  }
+        public void OnAfterDeserialize() { _runtimeValue = _initialValue; }
     }
 }
