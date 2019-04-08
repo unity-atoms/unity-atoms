3b0664fe04b2c088b037bf732e96ce95a816845c
Adam Ramberg <andersson.adam.89@gmail.com>
2019-04-08 18:43:29 +0200

    Minor style adjustments to CodeGenerator.cs

--- Source/Editor/CodeGenerator.cs
+++ Source/Editor/CodeGenerator.cs
@@ -5,74 +5,85 @@ using UnityEditor.Experimental.UIElements;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 using UnityEngine.Experimental.UIElements.StyleEnums;
+using UnityAtoms.Logger;
 
-namespace UnityAtoms {
-    public class CodeGenerator : EditorWindow{
+namespace UnityAtoms
+{
+    public class CodeGenerator : EditorWindow
+    {
 
-        [MenuItem("Tools/UnityAtoms/Code Gen Utility")]
-        static void Init() {
+        [MenuItem("Tools/Unity Atoms/Code Gen Utility")]
+        static void Init()
+        {
             var window = GetWindow<CodeGenerator>();
             window.position = new Rect(Screen.width / 2, Screen.height / 2, 300, 100);
-            // window.titleContent = new GUIContent("Code Generator");
             window.Show();
         }
 
-        private string typeName = "";
-        private static string writePath = "Assets/Source/Types/";
+        private string _typeName = "";
 
-        private void OnEnable() {
+        private static string _writePath = System.Environment.CurrentDirectory.Contains("unity-atoms/UnityAtomsTestsAndExamples")
+            ? "../Source/Types/" : "Assets/Source/Types/";
+
+        private void OnEnable()
+        {
             var root = this.GetRootVisualContainer();
 
-            var inlineVE = new VisualElement() {    style = { flexDirection = FlexDirection.Row }    };
-            inlineVE.Add(new Label() {text = "Write Path", style = {width = 100}});
-            var textfield = new TextField() {value = writePath, style = {flexGrow = 1}};
-            textfield.RegisterCallback<ChangeEvent<string>>(evt => writePath = evt.newValue);
+            var inlineVE = new VisualElement() { style = { flexDirection = FlexDirection.Row } };
+            inlineVE.Add(new Label() { text = "Write Path", style = { width = 100 } });
+            var textfield = new TextField() { value = _writePath, style = { flexGrow = 1 } };
+            textfield.RegisterCallback<ChangeEvent<string>>(evt => _writePath = evt.newValue);
             inlineVE.Add(textfield);
             root.Add(inlineVE);
 
-            inlineVE = new VisualElement() {    style = { flexDirection = FlexDirection.Row }    };
-            inlineVE.Add(new Label() {text = "Type Name", style = {width = 100}});
-            textfield = new TextField() {value = typeName, style = {flexGrow = 1}};
-            textfield.RegisterCallback<ChangeEvent<string>>(evt => typeName = evt.newValue);
+            inlineVE = new VisualElement() { style = { flexDirection = FlexDirection.Row } };
+            inlineVE.Add(new Label() { text = "Type Name", style = { width = 100 } });
+            textfield = new TextField() { value = _typeName, style = { flexGrow = 1 } };
+            textfield.RegisterCallback<ChangeEvent<string>>(evt => _typeName = evt.newValue);
             inlineVE.Add(textfield);
             root.Add(inlineVE);
 
 
-            inlineVE = new VisualElement() {
+            inlineVE = new VisualElement()
+            {
                 style = { flexDirection = FlexDirection.Row }
             };
 
-            var button1 = new Button(Close) { text = "Close" , style = {flexGrow = 1} };
+            var button1 = new Button(Close) { text = "Close", style = { flexGrow = 1 } };
             inlineVE.Add(button1);
 
-            var button2 = new Button(Generate) { text = "Generate" , style = {flexGrow = 1}};
+            var button2 = new Button(Generate) { text = "Generate", style = { flexGrow = 1 } };
             inlineVE.Add(button2);
 
             root.Add(inlineVE);
         }
 
-        private void Generate() {
-            if( string.IsNullOrEmpty(typeName) ) return;
-            Debug.Log("Generating! " + typeName);
+        private void Generate()
+        {
+            if (string.IsNullOrEmpty(_typeName))
+            {
+                AtomsLogger.Warning("You need to specify a type name. Aborting!");
+                return;
+            }
+            AtomsLogger.Log("Generating " + _typeName);
 
-            Directory.CreateDirectory(writePath + $"{Capitalize(typeName)}");
-
-            GenerateAction(typeName);
-            GenerateConstant(typeName);
-            GenerateEvent(typeName);
-            GenerateEvent2(typeName);
-            GenerateUnityEvent(typeName);
-            GenerateList(typeName);
-            GenerateListener(typeName);
-            GenerateReference(typeName);
-            GenerateVariable(typeName);
+            GenerateAction(_typeName);
+            GenerateConstant(_typeName);
+            GenerateEvent(_typeName);
+            GenerateEvent2(_typeName);
+            GenerateUnityEvent(_typeName);
+            GenerateList(_typeName);
+            GenerateListener(_typeName);
+            GenerateReference(_typeName);
+            GenerateVariable(_typeName);
 
             AssetDatabase.Refresh();
         }
 
         #region Util_Functions
 
-        private static string Capitalize(string s) {
+        private static string Capitalize(string s)
+        {
             if (string.IsNullOrEmpty(s))
                 return string.Empty;
 
@@ -81,9 +92,10 @@ namespace UnityAtoms {
             return new string(a);
         }
 
-        private static void CreateFile(string typeName, string TEMPLATE, string name, string filename) {
+        private static void CreateFile(string typeName, string TEMPLATE, string name, string filename)
+        {
             var content = TEMPLATE.Replace("{TYPENAME}", name).Replace("{TYPE}", typeName);
-            var path = writePath + $"{name}/{filename}";
+            var path = _writePath + $"{name}/{filename}";
             File.WriteAllText(path, content);
             AssetDatabase.ImportAsset(path);
         }
@@ -92,9 +104,8 @@ namespace UnityAtoms {
 
         #region Code_Files
 
-
-
-        private void GenerateAction(string typeName) {
+        private void GenerateAction(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
@@ -108,13 +119,14 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateConstant(string typeName) {
+        private void GenerateConstant(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
 [CreateAssetMenu(menuName = ""Unity Atoms/{TYPENAME}/Constant"", fileName = ""{TYPENAME}Constant"", order = CreateAssetMenuUtils.Order.CONSTANT)]
-    public class {TYPENAME}Constant : ScriptableObjectVariableBase<{TYPE}> {
+    public sealed class {TYPENAME}Constant : ScriptableObjectVariableBase<{TYPE}> {
     }
 }";
             var name = Capitalize(typeName);
@@ -123,13 +135,14 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateEvent(string typeName) {
+        private void GenerateEvent(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
 [CreateAssetMenu(menuName = ""Unity Atoms/{TYPENAME}/Event"", fileName = ""{TYPENAME}Event"", order = CreateAssetMenuUtils.Order.EVENT)]
-    public class {TYPENAME}Event : GameEvent<{TYPE}> {
+    public sealed class {TYPENAME}Event : GameEvent<{TYPE}> {
     }
 }";
             var name = Capitalize(typeName);
@@ -138,13 +151,14 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateEvent2(string typeName) {
+        private void GenerateEvent2(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
     [CreateAssetMenu(menuName = ""Unity Atoms/{TYPENAME}/Event x 2"", fileName = ""{TYPENAME}{TYPENAME}Event"", order = CreateAssetMenuUtils.Order.EVENTx2)]
-    public class {TYPENAME}{TYPENAME}Event : GameEvent<{TYPE}, {TYPE}> {
+    public sealed class {TYPENAME}{TYPENAME}Event : GameEvent<{TYPE}, {TYPE}> {
     }
 }";
             var name = Capitalize(typeName);
@@ -152,7 +166,8 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateUnityEvent(string typeName) {
+        private void GenerateUnityEvent(string typeName)
+        {
             const string TEMPLATE =
 @"using System;
 using UnityEngine;
@@ -160,7 +175,7 @@ using UnityEngine.Events;
 
 namespace UnityAtoms {
     [Serializable]
-    public class Unity{TYPENAME}Event : UnityEvent<{TYPE}> {
+    public sealed class Unity{TYPENAME}Event : UnityEvent<{TYPE}> {
     }
 }";
             var name = Capitalize(typeName);
@@ -168,13 +183,14 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateList(string typeName) {
+        private void GenerateList(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
 [CreateAssetMenu(menuName = ""Unity Atoms/{TYPENAME}/List"", fileName = ""{TYPENAME}List"", order = CreateAssetMenuUtils.Order.LIST)]
-    public class {TYPENAME}List : ScriptableObjectList<{TYPE}, {TYPENAME}Event> {
+    public sealed class {TYPENAME}List : ScriptableObjectList<{TYPE}, {TYPENAME}Event> {
     }
 }";
             var name = Capitalize(typeName);
@@ -183,12 +199,13 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateListener(string typeName) {
+        private void GenerateListener(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
-    public class {TYPENAME}Listener : GameEventListener<{TYPE}, {TYPENAME}Action, {TYPENAME}Event, Unity{TYPENAME}Event> {
+    public sealed class {TYPENAME}Listener : GameEventListener<{TYPE}, {TYPENAME}Action, {TYPENAME}Event, Unity{TYPENAME}Event> {
     }
 }";
             var name = Capitalize(typeName);
@@ -197,14 +214,15 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateReference(string typeName) {
+        private void GenerateReference(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 using System;
 
 namespace UnityAtoms {
     [Serializable]
-    public class {TYPENAME}Reference : ScriptableObjectReference<{TYPE}, {TYPENAME}Variable, {TYPENAME}Event, {TYPENAME}{TYPENAME}Event> {
+    public sealed class {TYPENAME}Reference : ScriptableObjectReference<{TYPE}, {TYPENAME}Variable, {TYPENAME}Event, {TYPENAME}{TYPENAME}Event> {
     }
 }";
             var name = Capitalize(typeName);
@@ -213,13 +231,14 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-        private void GenerateVariable(string typeName) {
+        private void GenerateVariable(string typeName)
+        {
             const string TEMPLATE =
 @"using UnityEngine;
 
 namespace UnityAtoms {
 [CreateAssetMenu(menuName = ""Unity Atoms/{TYPENAME}/Variable"", fileName = ""{TYPENAME}Variable"", order = CreateAssetMenuUtils.Order.VARIABLE)]
-    public class {TYPENAME}Variable : EquatableScriptableObjectVariable<{TYPE}, {TYPENAME}Event, {TYPENAME}{TYPENAME}Event> {
+    public sealed class {TYPENAME}Variable : EquatableScriptableObjectVariable<{TYPE}, {TYPENAME}Event, {TYPENAME}{TYPENAME}Event> {
     }
 }";
             var name = Capitalize(typeName);
@@ -228,8 +247,6 @@ namespace UnityAtoms {
             CreateFile(typeName, TEMPLATE, name, filename);
         }
 
-
         #endregion
     }
-
 }
