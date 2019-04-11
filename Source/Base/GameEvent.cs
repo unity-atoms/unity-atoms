d551947c13728ab9a754ced28a92e7d3e8a24b62
Adam Ramberg <andersson.adam.89@gmail.com>
2019-04-11 13:52:13 +0200

    Test using action for game events

--- Source/Base/GameEvent.cs
+++ Source/Base/GameEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,47 +6,41 @@ namespace UnityAtoms
 {
     public abstract class GameEvent<T> : ScriptableObject
     {
-        private readonly List<IGameEventListener<T>> _eventListeners = new List<IGameEventListener<T>>();
+        public event Action<T> OnEvent;
 
         public void Raise(T item)
         {
-            for (int i = _eventListeners.Count - 1; i >= 0; i--)
-                _eventListeners[i].OnEventRaised(item);
+            OnEvent?.Invoke(item);
         }
 
         public void RegisterListener(IGameEventListener<T> listener)
         {
-            if (!_eventListeners.Contains(listener))
-                _eventListeners.Add(listener);
+            OnEvent += listener.OnEventRaised;
         }
 
         public void UnregisterListener(IGameEventListener<T> listener)
         {
-            if (_eventListeners.Contains(listener))
-                _eventListeners.Remove(listener);
+            OnEvent -= listener.OnEventRaised;
         }
     }
 
     public abstract class GameEvent<T1, T2> : ScriptableObject
     {
-        private readonly List<IGameEventListener<T1, T2>> _eventListeners = new List<IGameEventListener<T1, T2>>();
+        public event Action<T1, T2> OnEvent;
 
         public void Raise(T1 item1, T2 item2)
         {
-            for (int i = _eventListeners.Count - 1; i >= 0; i--)
-                _eventListeners[i].OnEventRaised(item1, item2);
+            OnEvent?.Invoke(item1, item2);
         }
 
         public void RegisterListener(IGameEventListener<T1, T2> listener)
         {
-            if (!_eventListeners.Contains(listener))
-                _eventListeners.Add(listener);
+            OnEvent += listener.OnEventRaised;
         }
 
         public void UnregisterListener(IGameEventListener<T1, T2> listener)
         {
-            if (_eventListeners.Contains(listener))
-                _eventListeners.Remove(listener);
+            OnEvent -= listener.OnEventRaised;
         }
     }
 }
