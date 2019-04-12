deea018ce179a07c12c2a89d7201a5e050fc6476
Adam Ramberg <andersson.adam.89@gmail.com>
2019-04-12 10:19:40 +0200

    Dynamic actions and functions

--- Source/Utils/DynamicAtoms.cs
+++ Source/Utils/DynamicAtoms.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace UnityAtoms.Utils
@@ -24,6 +25,94 @@ namespace UnityAtoms.Utils
             sol.Removed = removed;
             sol.Cleared = cleared;
             return sol;
+        }
+
+        public static GA CreateAction<GA, T1>(Action<T1> action)
+            where GA : GameAction<T1>
+        {
+            var ga = ScriptableObject.CreateInstance<GA>();
+            ga.Action = action;
+            return ga;
+        }
+
+        public static GA CreateAction<GA, T1, T2>(Action<T1, T2> action)
+            where GA : GameAction<T1, T2>
+        {
+            var ga = ScriptableObject.CreateInstance<GA>();
+            ga.Action = action;
+            return ga;
+        }
+
+        public static GA CreateAction<GA, T1, T2, T3>(Action<T1, T2, T3> action)
+            where GA : GameAction<T1, T2, T3>
+        {
+            var ga = ScriptableObject.CreateInstance<GA>();
+            ga.Action = action;
+            return ga;
+        }
+
+        public static GA CreateAction<GA, T1, T2, T3, T4>(Action<T1, T2, T3, T4> action)
+            where GA : GameAction<T1, T2, T3, T4>
+        {
+            var ga = ScriptableObject.CreateInstance<GA>();
+            ga.Action = action;
+            return ga;
+        }
+
+        public static GA CreateAction<GA, T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action)
+            where GA : GameAction<T1, T2, T3, T4, T5>
+        {
+            var ga = ScriptableObject.CreateInstance<GA>();
+            ga.Action = action;
+            return ga;
+        }
+
+        public static GF CreateFunction<GF, R>(Func<R> func)
+            where GF : GameFunction<R>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
+        }
+
+        public static GF CreateFunction<GF, R, T1>(Func<T1, R> func)
+            where GF : GameFunction<R, T1>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
+        }
+
+        public static GF CreateFunction<GF, R, T1, T2>(Func<T1, T2, R> func)
+            where GF : GameFunction<R, T1, T2>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
+        }
+
+        public static GF CreateFunction<GF, R, T1, T2, T3>(Func<T1, T2, T3, R> func)
+            where GF : GameFunction<R, T1, T2, T3>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
+        }
+
+        public static GF CreateFunction<GF, R, T1, T2, T3, T4>(Func<T1, T2, T3, T4, R> func)
+            where GF : GameFunction<R, T1, T2, T3, T4>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
+        }
+
+        public static GF CreateFunction<GF, R, T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, R> func)
+            where GF : GameFunction<R, T1, T2, T3, T4, T5>
+        {
+            var gf = ScriptableObject.CreateInstance<GF>();
+            gf.Func = func;
+            return gf;
         }
     }
 }
