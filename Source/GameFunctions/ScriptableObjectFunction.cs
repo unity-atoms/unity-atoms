a98ad5be3b1962f74e6476618d8f71073df0b9ae
Adam Ramberg <andersson.adam.89@gmail.com>
2019-04-12 10:09:54 +0200

    Use C# Func / Action in GameActions / Funcs

--- Source/GameFunctions/ScriptableObjectFunction.cs
+++ Source/GameFunctions/ScriptableObjectFunction.cs
@@ -1,34 +1,101 @@
+using System;
 using UnityEngine;
 
 namespace UnityAtoms
 {
     public abstract class GameFunction<R> : ScriptableObject
     {
-        public abstract R Call();
+        [HideInInspector]
+        public Func<R> Func;
+
+        public virtual R Call()
+        {
+            if (Func != null)
+            {
+                return Func();
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 
     public abstract class GameFunction<R, T1> : ScriptableObject
     {
-        public abstract R Call(T1 t1);
+        [HideInInspector]
+        public Func<T1, R> func;
+
+        public virtual R Call(T1 t1)
+        {
+            if (func != null)
+            {
+                return func(t1);
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 
     public abstract class GameFunction<R, T1, T2> : ScriptableObject
     {
-        public abstract R Call(T1 t1, T2 t2);
+        [HideInInspector]
+        public Func<T1, T2, R> func;
+
+        public virtual R Call(T1 t1, T2 t2)
+        {
+            if (func != null)
+            {
+                return func(t1, t2);
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 
     public abstract class GameFunction<R, T1, T2, T3> : ScriptableObject
     {
-        public abstract R Call(T1 t1, T2 t2, T3 t3);
+        [HideInInspector]
+        public Func<T1, T2, T3, R> func;
+
+        public virtual R Call(T1 t1, T2 t2, T3 t3)
+        {
+            if (func != null)
+            {
+                return func(t1, t2, t3);
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 
     public abstract class GameFunction<R, T1, T2, T3, T4> : ScriptableObject
     {
-        public abstract R Call(T1 t1, T2 t2, T3 t3, T4 t4);
+        [HideInInspector]
+        public Func<T1, T2, T3, T4, R> func;
+
+        public virtual R Call(T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            if (func != null)
+            {
+                return func(t1, t2, t3, t4);
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 
     public abstract class GameFunction<R, T1, T2, T3, T4, T5> : ScriptableObject
     {
-        public abstract R Call(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5);
+        [HideInInspector]
+        public Func<T1, T2, T3, T4, T5, R> func;
+
+        public virtual R Call(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5)
+        {
+            if (func != null)
+            {
+                return func(t1, t2, t3, t4, t5);
+            }
+
+            throw new Exception("Either set Func or override the Call method.");
+        }
     }
 }
