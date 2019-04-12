1cabd33c74ceb43a62b1b95fa0b90e116697293e
Adam Ramberg <andersson.adam.89@gmail.com>
2019-04-12 10:19:25 +0200

    Correct spelling mistake in GameFunction

--- Source/GameFunctions/ScriptableObjectFunction.cs
+++ Source/GameFunctions/ScriptableObjectFunction.cs
@@ -22,13 +22,13 @@ namespace UnityAtoms
     public abstract class GameFunction<R, T1> : ScriptableObject
     {
         [HideInInspector]
-        public Func<T1, R> func;
+        public Func<T1, R> Func;
 
         public virtual R Call(T1 t1)
         {
-            if (func != null)
+            if (Func != null)
             {
-                return func(t1);
+                return Func(t1);
             }
 
             throw new Exception("Either set Func or override the Call method.");
@@ -38,13 +38,13 @@ namespace UnityAtoms
     public abstract class GameFunction<R, T1, T2> : ScriptableObject
     {
         [HideInInspector]
-        public Func<T1, T2, R> func;
+        public Func<T1, T2, R> Func;
 
         public virtual R Call(T1 t1, T2 t2)
         {
-            if (func != null)
+            if (Func != null)
             {
-                return func(t1, t2);
+                return Func(t1, t2);
             }
 
             throw new Exception("Either set Func or override the Call method.");
@@ -54,13 +54,13 @@ namespace UnityAtoms
     public abstract class GameFunction<R, T1, T2, T3> : ScriptableObject
     {
         [HideInInspector]
-        public Func<T1, T2, T3, R> func;
+        public Func<T1, T2, T3, R> Func;
 
         public virtual R Call(T1 t1, T2 t2, T3 t3)
         {
-            if (func != null)
+            if (Func != null)
             {
-                return func(t1, t2, t3);
+                return Func(t1, t2, t3);
             }
 
             throw new Exception("Either set Func or override the Call method.");
@@ -70,13 +70,13 @@ namespace UnityAtoms
     public abstract class GameFunction<R, T1, T2, T3, T4> : ScriptableObject
     {
         [HideInInspector]
-        public Func<T1, T2, T3, T4, R> func;
+        public Func<T1, T2, T3, T4, R> Func;
 
         public virtual R Call(T1 t1, T2 t2, T3 t3, T4 t4)
         {
-            if (func != null)
+            if (Func != null)
             {
-                return func(t1, t2, t3, t4);
+                return Func(t1, t2, t3, t4);
             }
 
             throw new Exception("Either set Func or override the Call method.");
@@ -86,13 +86,13 @@ namespace UnityAtoms
     public abstract class GameFunction<R, T1, T2, T3, T4, T5> : ScriptableObject
     {
         [HideInInspector]
-        public Func<T1, T2, T3, T4, T5, R> func;
+        public Func<T1, T2, T3, T4, T5, R> Func;
 
         public virtual R Call(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5)
         {
-            if (func != null)
+            if (Func != null)
             {
-                return func(t1, t2, t3, t4, t5);
+                return Func(t1, t2, t3, t4, t5);
             }
 
             throw new Exception("Either set Func or override the Call method.");
