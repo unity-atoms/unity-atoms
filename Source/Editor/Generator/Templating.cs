0f204234610af557c7ee8920f123a2ef70f1a44c
AdamRamberg <adam@mambojambostudios.com>
2019-06-17 23:11:00 +0200

    Small style tweaks

--- Source/Editor/Generator/Templating.cs
+++ Source/Editor/Generator/Templating.cs
@@ -5,33 +5,33 @@ namespace UnityAtoms.Editor
 {
     internal class Templating
     {
-
         public static string ResolveConditionals(string template, List<string> trueConditions)
         {
             var templateCopy = String.Copy(template);
-            
-            int indexIfOpened = templateCopy.LastIndexOf("<%IF ", StringComparison.Ordinal);
-            if (indexIfOpened == -1) return templateCopy; // there are no IF blocks anymore -> finished processing.
-            
+
+            var indexIfOpened = templateCopy.LastIndexOf("<%IF ", StringComparison.Ordinal);
+            if (indexIfOpened == -1) return templateCopy; // No IF blocks left and nothing else to resolve. Return template.
+
             var indexIfClosed = templateCopy.IndexOf("%>", indexIfOpened + 5, StringComparison.Ordinal);
-            if(indexIfClosed == -1)  throw new Exception("Found Opened <%IF block but it was never closed (missing %> )");
-            
+            if (indexIfClosed == -1) throw new Exception("Found <%IF block but it was never closed (missing %>)");
+
             var condition = templateCopy.Substring(indexIfOpened + 5, indexIfClosed - (indexIfOpened + 5));
             var isNegatedCondition = condition.Substring(0, 1) == "!";
             if (isNegatedCondition) { condition = condition.Substring(1); }
-            
+
             var indexOfNextEndIf = templateCopy.IndexOf("<%ENDIF%>", indexIfClosed, StringComparison.Ordinal);
-            if(indexOfNextEndIf == -1)  throw new Exception($"No closing <%ENDIF%> for condition .");
-            
+            if (indexOfNextEndIf == -1) throw new Exception("No closing <%ENDIF%> for condition.");
+
             var indexOfNextElse = templateCopy.IndexOf("<%ELSE%>", indexIfClosed, StringComparison.Ordinal);
 
-            var endThenBlock = indexOfNextElse != -1 ? indexOfNextElse : indexOfNextEndIf; 
-            
-            string resolved = "";
+            var endThenBlock = indexOfNextElse != -1 ? indexOfNextElse : indexOfNextEndIf;
+
+            var resolved = "";
             if (trueConditions.Contains(condition) ^ isNegatedCondition)
             {
                 resolved = templateCopy.Substring(indexIfClosed + 2, endThenBlock - (indexIfClosed + 2));
-            }else if (indexOfNextElse != -1)
+            }
+            else if (indexOfNextElse != -1)
             {
                 resolved = templateCopy.Substring(indexOfNextElse + 8, indexOfNextEndIf - (indexOfNextElse + 8));
             }
@@ -39,7 +39,7 @@ namespace UnityAtoms.Editor
             resolved = resolved.Trim('\n');
             templateCopy = templateCopy.Remove(indexIfOpened, (indexOfNextEndIf + 9) - indexIfOpened);
             templateCopy = templateCopy.Insert(indexIfOpened, resolved);
-            
+
             return ResolveConditionals(templateCopy, trueConditions);
         }
     }
