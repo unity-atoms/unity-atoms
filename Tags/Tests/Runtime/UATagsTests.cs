94dac2aa3075497a542c8f181458f464079f2750
AdamRamberg <adam@mambojambostudios.com>
2019-10-15 02:08:41 +0200

    Generate docs - document tags

--- Tags/Tests/Runtime/UATagsTests.cs
+++ Tags/Tests/Runtime/UATagsTests.cs
@@ -22,7 +22,7 @@ namespace UnityAtoms.Tags.Tests
         public IEnumerator TestingAndProfiling()
         {
             var go = new GameObject();
-            var atomicTags = go.AddComponent<UnityAtomsTags>();
+            var atomicTags = go.AddComponent<UATags>();
 
             List<string> random_tags_raw = new List<string>() { "a", "c", "e", "g", "i", "k", "m" };
             random_tags_raw.OrderBy((s => Random.value)).ToList();
@@ -52,10 +52,10 @@ namespace UnityAtoms.Tags.Tests
             Assert.That(() => { var t1 = atomicTags.Tags[2].Value; }, Is.Not.AllocatingGCMemory());
             Assert.That(() => { atomicTags.HasTag(null); }, Is.Not.AllocatingGCMemory());
 
-            Assert.AreSame(go, UnityAtomsTags.FindByTag("e"));
+            Assert.AreSame(go, UATags.FindByTag("e"));
             using (new ProfilerMarker("MySystem.FindByTag").Auto())
             {
-                UnityAtomsTags.FindByTag("e");
+                UATags.FindByTag("e");
             }
             // THIS:
             // Assert.That(() => { var t1 = AtomicTags.FindByTag("e"); }, Is.AllocatingGCMemory());
@@ -71,7 +71,7 @@ namespace UnityAtoms.Tags.Tests
 
             using (new ProfilerMarker("MySystem.GetGlobal").Auto())
             {
-                UnityAtomsTags.GetForGameObject(go);
+                UATags.GetTagsForGameObject(go);
             }
 
             Assert.AreEqual(random_tags_raw.Count, atomicTags.Tags.Count);
