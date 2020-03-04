05d3549dcef6205b7377c8f0b1d177f2268aa67b
Adam Ramberg <adam@mambojambostudios.com>
2020-03-05 00:56:56 +0100

    Re add accidently deleted stuff

--- Packages/BaseAtoms/Runtime/References/Collider2DReference.cs
+++ Packages/BaseAtoms/Runtime/References/Collider2DReference.cs
@@ -25,7 +25,7 @@ namespace UnityAtoms.BaseAtoms
         public bool Equals(Collider2DReference other) { return base.Equals(other); }
         protected override bool ValueEquals(Collider2D other)
         {
-            throw new NotImplementedException();
-        } 
+            return (this.Value == null && other == null) || this.Value != null && other != null && this.Value == other;
+        }
     }
 }
--- Packages/BaseAtoms/Runtime/References/ColliderReference.cs
+++ Packages/BaseAtoms/Runtime/References/ColliderReference.cs
@@ -25,7 +25,7 @@ namespace UnityAtoms.BaseAtoms
         public bool Equals(ColliderReference other) { return base.Equals(other); }
         protected override bool ValueEquals(Collider other)
         {
-            throw new NotImplementedException();
-        } 
+            return (this.Value == null && other == null) || this.Value != null && other != null && this.Value == other;
+        }
     }
 }
