55d96c22500fc6732cd72d84df17657f1933688e
Adam Ramberg <adam@mambojambostudios.com>
2020-04-03 15:45:08 +0200

    #122 Add Event Refs for Collections and Lists (#123)

--- Packages/Core/Editor/Generator/RegenerateAllAtoms.cs
+++ Packages/Core/Editor/Generator/RegenerateAllAtoms.cs
@@ -86,7 +86,7 @@ namespace UnityAtoms.Editor
                     valueType: "AtomBaseVariable",
                     baseWritePath: Path.Combine(path, "BaseAtoms"),
                     isValueEquatable: false,
-                    atomTypesToGenerate: new List<AtomType>() { AtomTypes.EVENT, AtomTypes.ACTION, AtomTypes.UNITY_EVENT, AtomTypes.EVENT_LISTENER },
+                    atomTypesToGenerate: new List<AtomType>() { AtomTypes.EVENT, AtomTypes.ACTION, AtomTypes.UNITY_EVENT, AtomTypes.BASE_EVENT_REFERENCE, AtomTypes.EVENT_INSTANCER, AtomTypes.BASE_EVENT_REFERENCE_LISTENER },
                     typeNamespace: "",
                     subUnityAtomsNamespace: "BaseAtoms"
                 ),
