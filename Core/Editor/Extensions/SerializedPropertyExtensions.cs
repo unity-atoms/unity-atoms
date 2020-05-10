14d18d91523c797ce2723f1b47346b041ab3c51a
AdamRamberg <adam@mambojambostudios.com>
2020-05-10 16:27:46 +0200

    Add GetParent / GetValue extenstions for SerializedProperty

--- Core/Editor/Extensions/SerializedPropertyExtensions.cs
+++ Core/Editor/Extensions/SerializedPropertyExtensions.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Reflection;
+using System.Linq;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -120,6 +122,52 @@ namespace UnityAtoms.Editor
             {
                 property.DeleteArrayElementAtIndex(index);
             }
+        }
+
+        public static object GetParent(this SerializedProperty property)
+        {
+            var path = property.propertyPath.Replace(".Array.data[", "[");
+            object obj = property.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements.Take(elements.Length - 1))
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    obj = obj.GetValue(elementName, index);
+                }
+                else
+                {
+                    obj = obj.GetValue(element);
+                }
+            }
+            return obj;
+        }
+
+        public static object GetValue(this object source, string name)
+        {
+            if (source == null)
+                return null;
+            var type = source.GetType();
+            var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (f == null)
+            {
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p == null)
+                    return null;
+                return p.GetValue(source, null);
+            }
+            return f.GetValue(source);
+        }
+
+        public static object GetValue(this object source, string name, int index)
+        {
+            var enumerable = GetValue(source, name) as IEnumerable;
+            var enm = enumerable.GetEnumerator();
+            while (index-- >= 0)
+                enm.MoveNext();
+            return enm.Current;
         }
     }
 }
