2b07654539ec5d5a5e5c626b77fc45244af5bc0f
jmacgill <jmacgill@google.com>
2020-06-24 12:15:28 -0400

    Make IsUnityAtomsRepo OS agnostic (#168)
    * Make Examples path check OS agnostic.
    
    Co-authored-by: jrmacgill <jmacgill@gmail.com>

--- Core/Runtime/Utils/Runtime.cs
+++ Core/Runtime/Utils/Runtime.cs
@@ -1,4 +1,6 @@
-﻿namespace UnityAtoms
+﻿using System.IO;
+
+namespace UnityAtoms
 {
     /// <summary>
     /// Internal constant and static readonly members for runtime usage.
@@ -31,7 +33,7 @@
         /// <returns>`true` if we are working in the Unity Atoms source library / repo, otherwise `false`.</returns>
         public static bool IsUnityAtomsRepo
         {
-            get => System.Environment.CurrentDirectory.Contains("unity-atoms/Examples");
+            get => System.Environment.CurrentDirectory.Contains(Path.Combine("unity-atoms","Examples"));
         }
     }
 }
