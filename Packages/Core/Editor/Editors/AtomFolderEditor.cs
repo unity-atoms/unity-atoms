c4fa9a2f149a6b2b19f099858aed515ab51f9a72
Soraphis <soraphis@gmail.com>
2020-08-07 18:31:26 +0200

    added folder inspector to allow UnityAtoms specific actions and shortcuts to improve usability

---
+++ Packages/Core/Editor/Editors/AtomFolderEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace UnityAtoms.Editor
+{
+    [CustomEditor(typeof(DefaultAsset))]
+    public class AtomFolderEditor : UnityEditor.Editor
+    {
+        protected override void OnHeaderGUI()
+        {
+            base.OnHeaderGUI();
+
+            if(! Directory.Exists(AssetDatabase.GetAssetPath(target))) return;
+            if (!target.name.ToLowerInvariant().Contains("atom")) return;
+
+
+            GUILayout.Label("UNITY ATOMS", (GUIStyle)"AC BoldHeader", GUILayout.ExpandWidth(true));
+
+            if (GUILayout.Button("Add Generator", (GUIStyle)"LargeButton"))
+            {
+                var asset = CreateInstance<AtomGenerator>();
+                var newPath = Path.Combine(
+                    AssetDatabase.GetAssetPath(target),
+                    "New Atom Type.asset"
+                );
+                Debug.Log(newPath);
+                AssetDatabase.CreateAsset(asset, newPath);
+                AssetDatabase.ImportAsset(newPath);
+            }
+        }
+
+    }
+}
