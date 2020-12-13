9154461984f4518080f6ab228dc385502b1b21f3
Casey Hofland <hofland.casey@gmail.com>
2020-12-13 21:34:50 +0100

    Add the PlayerInput and CallbackContext to RegenerateAllAtoms
    With it, all the atoms generated from PlayerInput and CallbackContext have been regenerated as well, so this push may appear bigger than it really is. Only RegenerateAllAtoms.cs has changed significantly.

--- Packages/InputSystem/Runtime/Events/PlayerInputEvent.cs
+++ Packages/InputSystem/Runtime/Events/PlayerInputEvent.cs
@@ -7,7 +7,7 @@ namespace UnityAtoms.InputSystem
     /// Event of type `PlayerInput`. Inherits from `AtomEvent&lt;PlayerInput&gt;`.
     /// </summary>
     [EditorIcon("atom-icon-cherry")]
-    [CreateAssetMenu(menuName = "Unity Atoms/Input System/Events/Player Input", fileName = "PlayerInputEvent")]
+    [CreateAssetMenu(menuName = "Unity Atoms/Events/PlayerInput", fileName = "PlayerInputEvent")]
     public sealed class PlayerInputEvent : AtomEvent<PlayerInput>
     {
     }
