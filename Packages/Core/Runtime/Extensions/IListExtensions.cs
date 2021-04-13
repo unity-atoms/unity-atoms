c9026104925a337ddefe9a9accce315aa2026419
Andrew Menino-Barlow <andrew.barlow@gmail.com>
2021-04-13 13:43:12 +0100

    Rollback changes in IListExtensions.cs. Note: Exist<T>() and GetOrInstantiate() don't look like they're called anywhere, so are a candidate for removal.

--- Packages/Core/Runtime/Extensions/IListExtensions.cs
+++ Packages/Core/Runtime/Extensions/IListExtensions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace UnityAtoms
 {
@@ -10,15 +9,17 @@ namespace UnityAtoms
         public static T First<T>(this IList<T> list, Func<T, bool> func)
         {
             for (int i = 0; list != null && i < list.Count; ++i)
+            {
                 if (func(list[i])) return list[i];
+            }
 
             return default(T);
         }
 
         public static bool Exists<T>(this IList<T> list, Func<T, bool> func)
         {
-            var first = list.First(func);
-            return first is { };
+            var first = list.First<T>(func);
+            return first != null ? true : false;
         }
 
         public static GameObject GetOrInstantiate(this IList<GameObject> list, UnityEngine.Object prefab, Vector3 position, Quaternion quaternion, Func<GameObject, bool> condition)
@@ -32,7 +33,7 @@ namespace UnityAtoms
                 return component;
             }
 
-            var newGameObject = Object.Instantiate(prefab, position, quaternion) as GameObject;
+            var newGameObject = GameObject.Instantiate(prefab, position, quaternion) as GameObject;
             list.Add(newGameObject);
             return newGameObject;
         }
