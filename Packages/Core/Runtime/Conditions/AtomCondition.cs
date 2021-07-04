c2dd13712f2924bf5faf769d0a5d04a453ad2b96
Miika Lönnqvist <miika.lonnqvist@gmail.com>
2021-07-04 11:18:46 +0200

    Fix conditions not serializing in Unity 2019 (#278)

--- Packages/Core/Runtime/Conditions/AtomCondition.cs
+++ Packages/Core/Runtime/Conditions/AtomCondition.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 namespace UnityAtoms
 {
     /// <summary>
-    /// Base abstract class for Conditions. Condition must be an AtomFunction&lt;bool, T&gt;.
+    /// Base abstract class for Conditions. Inherits from `BaseAtom`.
     /// </summary>
-    public abstract class AtomCondition<T> : AtomFunction<bool, T>
+    public abstract class AtomCondition : BaseAtom
     {
+        /// <summary>
+        /// Check the condition.
+        /// </summary>
+        public virtual bool Call() => false;
+    }
+
+    /// <summary>
+    /// Generic abstract base class for Conditions. Inherits from `AtomCondition`.
+    /// </summary>
+    public abstract class AtomCondition<T> : AtomCondition
+    {
+        /// <summary>
+        /// Evaluate the Condition.
+        /// </summary>
+        /// <param name="t1">The first parameter.</param>
+        public virtual bool Call(T t) => base.Call();
     }
 }
