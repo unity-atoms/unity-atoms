1d498090e391c1495b8de8f8b11e8bda142bd710
Soraphis <soraphis@users.noreply.github.com>
2022-10-24 00:22:30 +0200

    prevent null reference exceptions in editor when using not-serializab… (#371)
    * prevent null reference exceptions in editor when using not-serializable types
    
    * - Draw warning helpbox only once in AtomVariableEditor.cs
    - Minor clean up
    
    * Revert unintended removed line
    
    Co-authored-by: Adam Ramberg <adam@mambojambostudios.com>

--- Packages/Core/Editor/Drawers/AtomBaseReferenceDrawer.cs
+++ Packages/Core/Editor/Drawers/AtomBaseReferenceDrawer.cs
@@ -35,7 +35,10 @@ namespace UnityAtoms.Editor
                 }
             }
 
-            return EditorGUI.GetPropertyHeight(property.FindPropertyRelative(usageData.PropertyName), label);
+            var innerProperty = property.FindPropertyRelative(usageData.PropertyName);
+            return innerProperty == null ?
+                EditorGUIUtility.singleLineHeight :
+                EditorGUI.GetPropertyHeight(innerProperty, label);
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
@@ -71,20 +74,26 @@ namespace UnityAtoms.Editor
             var usageTypePropertyName = GetUsages(property)[newUsageValue].PropertyName;
             var usageTypeProperty = property.FindPropertyRelative(usageTypePropertyName);
 
-            var expanded = usageTypeProperty.isExpanded;
-            usageTypeProperty.isExpanded = true;
-            var valueFieldHeight = EditorGUI.GetPropertyHeight(usageTypeProperty, label);
-            usageTypeProperty.isExpanded = expanded;
-
-            if (usageTypePropertyName == "_value" && valueFieldHeight > EditorGUIUtility.singleLineHeight + 2)
+            if (usageTypeProperty == null)
             {
-                EditorGUI.PropertyField(originalPosition, usageTypeProperty, GUIContent.none, true);
+                EditorGUI.LabelField(position, "[Non serialized value]");
             }
             else
             {
-                EditorGUI.PropertyField(position, usageTypeProperty, GUIContent.none);
-            }
+                var expanded = usageTypeProperty.isExpanded;
+                usageTypeProperty.isExpanded = true;
+                var valueFieldHeight = EditorGUI.GetPropertyHeight(usageTypeProperty, label);
+                usageTypeProperty.isExpanded = expanded;
 
+                if (usageTypePropertyName == "_value" && (valueFieldHeight > EditorGUIUtility.singleLineHeight + 2))
+                {
+                    EditorGUI.PropertyField(originalPosition, usageTypeProperty, GUIContent.none, true);
+                }
+                else
+                {
+                    EditorGUI.PropertyField(position, usageTypeProperty, GUIContent.none);
+                }
+            }
             if (EditorGUI.EndChangeCheck())
                 property.serializedObject.ApplyModifiedProperties();
 
