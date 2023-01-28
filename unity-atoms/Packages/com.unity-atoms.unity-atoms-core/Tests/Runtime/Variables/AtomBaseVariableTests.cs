6537ec8343a7156de9a76ff4d7dcdfda3bc01434
miikalo <1841625+miikalo@users.noreply.github.com>
2023-01-28 19:00:48 +0100

    Add more AtomBaseVariable tests

--- unity-atoms/Packages/com.unity-atoms.unity-atoms-core/Tests/Runtime/Variables/AtomBaseVariableTests.cs
+++ unity-atoms/Packages/com.unity-atoms.unity-atoms-core/Tests/Runtime/Variables/AtomBaseVariableTests.cs
@@ -2,9 +2,22 @@ using NUnit.Framework;
 using UnityAtoms;
 using FluentAssertions;
 using NSubstitute;
+using System;
 
 public class AtomBaseVariableTests
 {
+    [Test]
+    public void Is_abstract()
+    {
+        typeof(AtomBaseVariable).Should().BeAbstract();
+    }
+
+    [Test]
+    public void Generic_is_abstract()
+    {
+        typeof(AtomBaseVariable<dynamic>).Should().BeAbstract();
+    }
+
     [Test]
     public void Has_EditorIcon_attribute()
     {
@@ -12,12 +25,18 @@ public class AtomBaseVariableTests
     }
 
     [Test]
+    public void Generic_has_EditorIcon_attribute()
+    {
+        typeof(AtomBaseVariable<dynamic>).Should().BeDecoratedWith<EditorIcon>();
+    }
+
+    [Test]
     public void Can_be_equated()
     {
-        var a = Substitute.For<AtomBaseVariable<dynamic>>();
-        var b = Substitute.For<AtomBaseVariable<dynamic>>();
+        var _a = Substitute.For<AtomBaseVariable<dynamic>>();
+        var _b = Substitute.For<AtomBaseVariable<dynamic>>();
 
-        Assert.True(a.Equals(a));
-        Assert.False(a.Equals(b));
+        Assert.True(_a.Equals(_a));
+        Assert.False(_a.Equals(_b));
     }
 }
