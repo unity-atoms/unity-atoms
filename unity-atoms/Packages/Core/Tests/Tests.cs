3edafe1314e6e1794d3e42722683608a34be4a53
miikalo <1841625+miikalo@users.noreply.github.com>
2023-01-28 18:58:47 +0100

    Convert to a Unity project and add tests
    Moved Unity project to a new directory and edited relevant files to match the change.

---
+++ unity-atoms/Packages/Core/Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityAtoms;
+
+namespace Core
+{
+    public class Tests
+    {
+        // Tests for the Core classes of Unity Atoms
+    }
+}
