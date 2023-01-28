349a8772e1912c63ea1a28ab7b707ad33648aee6
miikalo <1841625+miikalo@users.noreply.github.com>
2023-01-28 19:00:40 +0100

    Add AtomListAttribute tests

---
+++ unity-atoms/Packages/com.unity-atoms.unity-atoms-core/Tests/Runtime/Attributes/AtomListAttributeTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using UnityAtoms;
+using NSubstitute;
+
+public class AtomListAttributeTests
+{
+    [Test]
+    public void AtomListAttribute_assigns_correct_properties()
+    {
+        var _atomListAttribute = Substitute.For<AtomListAttribute>();
+    }
+}
