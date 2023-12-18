fcaf6d49183dc172308d5175d07500bc12cf8d3f
Adam Ramberg <adam@mambojambostudios.com>
2023-12-18 21:32:04 +0100

    Added comment to IAtomInstancer

--- Packages/Core/Runtime/VariableInstancers/IAtomInstancer.cs
+++ Packages/Core/Runtime/VariableInstancers/IAtomInstancer.cs
@@ -1,7 +1,5 @@
 namespace UnityAtoms
 {
-    public interface IAtomInstancer
-    {
-        
-    }
+    // Currently only used in AtomBaseReferenceDrawer in order to auto set usage type on drag and drop
+    public interface IAtomInstancer { }
 }
