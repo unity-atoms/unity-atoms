629a82fec3de699a018f7dcb907aecd34060ee8b
Oliver Biwer <soraphis@googlemail.com>
2025-05-26 22:06:32 +0200

    feat: Added custom drawer for GameObjectValueList, that way it does not show "Type Missmatch" anymore when referencing runtime Gameobjects. feat: started adding documentation and a few video clips for ValueLists

---
+++ Packages/BaseAtoms/Editor/Editors/ValueLists/GameObjectValueListEditor.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace UnityAtoms.Editor
+{
+
+    [CustomEditor(typeof(GameObjectValueList))]
+    public class GameObjectValueListEditor : UnityEditor.Editor
+    {
+        ReorderableList list;
+
+        private void OnEnable()
+        {
+            InitList();
+
+            // register on playmode state change to clear the list / redraw:
+            UnityEditor.EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            UnityEditor.EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private void InitList()
+        {
+            list = new ReorderableList(serializedObject, serializedObject.FindProperty("list"), true, true, true, true);
+            list.drawHeaderCallback = (Rect rect) =>
+            {
+                EditorGUI.LabelField(rect, serializedObject.FindProperty("list").displayName);
+            };
+            list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                var element = list.serializedProperty.GetArrayElementAtIndex(index);
+                if (element.objectReferenceValue == null)
+                {
+                    EditorGUI.PropertyField(rect, element, GUIContent.none);
+                }
+                else
+                {
+                    rect.y += 2;
+                    rect.height -= 4;
+
+                    var objectRef = element.objectReferenceValue;
+                    var objectName = objectRef ? objectRef.name : "None (GameObject)";
+                    var icon = EditorGUIUtility.ObjectContent(objectRef, typeof(GameObject)).image;
+
+                    // Reserve space for the picker button on the right
+                    float pickerWidth = 18f;
+                    var pickerRect = new Rect(rect.xMax - pickerWidth, rect.y, pickerWidth, rect.height);
+
+                    // Reserve space for the icon on the left
+                    float iconSize = rect.height;
+                    var iconRect = new Rect(rect.x+4, rect.y+2, iconSize, iconSize-4);
+
+                    // Text button area (excluding icon and picker)
+                    float textPadding = 4f;
+                    var textRect = new Rect(rect.x + iconSize + textPadding, rect.y, rect.width - iconSize - pickerWidth - 2 * textPadding, rect.height);
+
+                    // Picker button (mimics the small circle at the end)
+                    if (GUI.Button(pickerRect, GUIContent.none, "buttonRight"))
+                    {
+                        EditorGUIUtility.ShowObjectPicker<GameObject>(objectRef, true, "", GUIUtility.GetControlID(FocusType.Passive));
+                    }
+
+                    // Draw button styled like ObjectField
+                    Rect objectFieldButtonRect = new Rect(rect.x, rect.y, rect.width - 2, rect.height);
+                    if (GUI.Button(objectFieldButtonRect, GUIContent.none, "IN ObjectField"))
+                    {
+                        if (objectRef != null)
+                            EditorGUIUtility.PingObject(objectRef);
+                    }
+
+                    Rect objectFieldRect = new Rect(rect.x, rect.y, rect.width - pickerWidth, rect.height);
+                    GUI.Label(objectFieldRect, GUIContent.none, EditorStyles.objectField);
+
+                    if (icon != null) GUI.DrawTexture(iconRect, icon, ScaleMode.ScaleToFit);
+                    GUI.Label(textRect, objectName);
+
+
+                    // Handle selection from picker
+                    if (Event.current.commandName == "ObjectSelectorUpdated")
+                    {
+                        var picked = EditorGUIUtility.GetObjectPickerObject();
+                        if (picked != element.objectReferenceValue)
+                        {
+                            element.objectReferenceValue = picked;
+                            GUI.changed = true;
+                        }
+                    }
+
+                    // this custom drag and drop logic still allows only asset-references to be dragged and dropped
+                    if (objectFieldRect.Contains(Event.current.mousePosition))
+                    {
+                        var evt = Event.current;
+                        switch (evt.type)
+                        {
+                            case EventType.DragUpdated:
+                            case EventType.DragPerform:
+                                // Validate dragged objects
+                                var dragged = DragAndDrop.objectReferences;
+
+                                bool valid = dragged.Length > 0 && dragged[0] is GameObject;
+                                if (valid)
+                                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                                else
+                                    DragAndDrop.visualMode = DragAndDropVisualMode.Rejected;
+
+                                if (evt.type == EventType.DragPerform && valid)
+                                {
+                                    DragAndDrop.AcceptDrag();
+                                    element.objectReferenceValue = dragged[0];
+                                    GUI.changed = true;
+                                }
+
+                                evt.Use();
+                                break;
+                        }
+                    }
+                }
+            };
+            list.onChangedCallback = (ReorderableList l) =>
+            {
+                // This is called when the list is changed, e.g. items are added or removed.
+                serializedObject.ApplyModifiedProperties();
+            };
+        }
+
+        private void OnDisable()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+        }
+
+        private void OnPlayModeStateChanged(PlayModeStateChange obj)
+        {
+            if (obj == PlayModeStateChange.EnteredPlayMode || obj == PlayModeStateChange.ExitingEditMode)
+            {
+                InitList();
+                serializedObject.Update();
+                Repaint();
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+
+            // Iterate through all serialized properties and draw them by default:
+            var serializedProperty = serializedObject.GetIterator();
+
+            serializedProperty.NextVisible(true); // Skip the first property (usually the name)
+            while (serializedProperty.NextVisible(false))
+            {
+                if (serializedProperty.name != "list")
+                {
+                    EditorGUILayout.PropertyField(serializedProperty, true);
+                }
+                else
+                {
+                    list?.DoLayoutList();
+                }
+            }
+
+        }
+    }
+}
